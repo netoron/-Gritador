d8ee5b8248d46528782304d94ccdde0fb5c8d398
LucasZeroCool <lucas.10.nunes@live.com>
2020-03-13 20:47:52 -0300

    Adicionando exercício

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira conteúdo minúsculo");
+            string conteúdo;
+            conteúdo = Console.ReadLine();
+            Console.WriteLine(conteúdo.ToUpper());
+
+            Console.WriteLine ("Gta melhor que minecraft");
+        }
+    }
+}
